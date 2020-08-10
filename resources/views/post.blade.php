ec40ea7c5fb8b3c5409b564969f170b53d089c6b
Duc <ducbuiquangxd@gmail.com>
2020-08-10 10:07:21 +0700

    Lesson: Routing 3

---
+++ resources/views/post.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <title>Tutorial</title>
+</head>
+<body>
+    <h1>My blog post</h1>
+
+    <p>{{ $post }}</p>
+</body>
+</html>
