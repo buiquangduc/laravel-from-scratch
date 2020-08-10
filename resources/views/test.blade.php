c9a6cd7481a7b55bf53d80e5d6288a9ea837aecb
Duc <ducbuiquangxd@gmail.com>
2020-08-10 09:30:12 +0700

    Lesson: Routing 1

---
+++ resources/views/test.blade.php
@@ -0,0 +1,1 @@
+<h1>Test complete</h1>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Tutorial
                 </div>
 
                 <div class="links">
