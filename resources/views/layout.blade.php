bc0528716973a18ab43315c7b5d0874235ba374f
Duc <ducbuiquangxd@gmail.com>
2020-08-11 08:53:25 +0700

    Lesson: Layout 4

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -19,6 +19,7 @@ Released   : 20140225
     <link href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900" rel="stylesheet" />
     <link href="css/default.css" rel="stylesheet" type="text/css" media="all" />
     <link href="css/fonts.css" rel="stylesheet" type="text/css" media="all" />
+    <link href="css/app.css" rel="stylesheet" type="text/css" media="all" />
 </head>
 <body>
 <div id="header-wrapper">
@@ -28,7 +29,7 @@ Released   : 20140225
         </div>
         <div id="menu">
             <ul>
-                <li class={{ Request::path() === '/' ? 'current_page_item' : ''}}><a href="#" accesskey="1" title="">Homepage</a></li>
+                <li class={{ Request::path() === '/' ? 'current_page_item' : ''}}><a href="/" accesskey="1" title="">Homepage</a></li>
                 <li class={{ Request::path() === 'clients' ? 'current_page_item' : ''}}><a href="#" accesskey="2" title="">Our Clients</a></li>
                 <li class={{ Request::path() === 'about' ? 'current_page_item' : ''}}><a href="/about" accesskey="3" title="">About Us</a></li>
                 <li class={{ Request::path() === 'careers' ? 'current_page_item' : ''}}><a href="#" accesskey="4" title="">Careers</a></li>
@@ -40,5 +41,8 @@ Released   : 20140225
     @yield('header')
 </div>
 @yield('content')
+<div id="copyright" class="container">
+    <p>&copy; Untitled. All rights reserved. | Photos by <a href="http://fotogrph.com/">Fotogrph</a> | Design by <a href="http://templated.co" rel="nofollow">TEMPLATED</a>.</p>
+</div>
 </body>
 </html>
