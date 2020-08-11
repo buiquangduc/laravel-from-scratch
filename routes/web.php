4b280e7258f0f9fd071629afa78047dce68c22e6
Duc <ducbuiquangxd@gmail.com>
2020-08-11 09:31:54 +0700

    Lesson: Layout 6

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,5 @@ Route::get('/', function() {
 Route::get('/about', function() {
     return view('about', ['articles' => \App\Article::latest()->take(3)->get()]);
 });
+
+Route::get('/articles/{article}', 'ArticleController@show');
