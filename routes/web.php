1f97e05f5a27c7a7cfea66b37acc1435b72e827b
Duc <ducbuiquangxd@gmail.com>
2020-08-12 15:50:35 +0700

    Lesson: Authentication 1

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,6 @@ Route::get('/', function() {
     return view('welcome');
 });
 
-Route::get('/about', function() {
-    return view('about', ['articles' => \App\Article::latest()->take(3)->get()]);
-});
+Auth::routes();
 
-Route::get('/articles', 'ArticleController@index')->name('articles.index');
-Route::get('/articles/create', 'ArticleController@create')->name('articles.create');
-Route::post('/articles', 'ArticleController@store')->name('articles.store');
-Route::get('/articles/{article}/edit', 'ArticleController@edit')->name('articles.edit');
-Route::put('/articles/{article}', 'ArticleController@update')->name('articles.update');
-Route::get('/articles/{article}', 'ArticleController@show')->name('articles.show');
+Route::get('/home', 'HomeController@index')->name('home');
